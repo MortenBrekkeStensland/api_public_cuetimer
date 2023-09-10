61d048a0712d9da3653f8aedcc747b2669a5e4d9
Mohammad Ahmad <eng.ma90@gmail.com>
2023-09-10 18:39:09 +0300

    Added a demo project

---
+++ CueTimerAPIClient/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace CueTimerAPIClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.textBoxFeedback = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.labelCommand = new System.Windows.Forms.Label();
+            this.labelFeedBack = new System.Windows.Forms.Label();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonFireNext = new System.Windows.Forms.Button();
+            this.buttonFireTimerWithID = new System.Windows.Forms.Button();
+            this.labelExampleCommands = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(152, 16);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(57, 20);
+            this.textBoxPort.TabIndex = 0;
+            this.textBoxPort.Text = "4778";
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(120, 82);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(196, 20);
+            this.textBoxCommand.TabIndex = 1;
+            // 
+            // textBoxFeedback
+            // 
+            this.textBoxFeedback.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFeedback.Location = new System.Drawing.Point(120, 108);
+            this.textBoxFeedback.Multiline = true;
+            this.textBoxFeedback.Name = "textBoxFeedback";
+            this.textBoxFeedback.Size = new System.Drawing.Size(319, 242);
+            this.textBoxFeedback.TabIndex = 2;
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(120, 19);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(26, 13);
+            this.labelPort.TabIndex = 3;
+            this.labelPort.Text = "Port";
+            // 
+            // labelCommand
+            // 
+            this.labelCommand.AutoSize = true;
+            this.labelCommand.Location = new System.Drawing.Point(14, 85);
+            this.labelCommand.Name = "labelCommand";
+            this.labelCommand.Size = new System.Drawing.Size(92, 13);
+            this.labelCommand.TabIndex = 4;
+            this.labelCommand.Text = "Custom Command";
+            // 
+            // labelFeedBack
+            // 
+            this.labelFeedBack.AutoSize = true;
+            this.labelFeedBack.Location = new System.Drawing.Point(14, 111);
+            this.labelFeedBack.Name = "labelFeedBack";
+            this.labelFeedBack.Size = new System.Drawing.Size(55, 13);
+            this.labelFeedBack.TabIndex = 5;
+            this.labelFeedBack.Text = "Feedback";
+            // 
+            // labelIP
+            // 
+            this.labelIP.AutoSize = true;
+            this.labelIP.Location = new System.Drawing.Point(12, 19);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(17, 13);
+            this.labelIP.TabIndex = 7;
+            this.labelIP.Text = "IP";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(35, 16);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(79, 20);
+            this.textBoxIP.TabIndex = 6;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(215, 14);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 8;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(322, 80);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 9;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.Location = new System.Drawing.Point(296, 14);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 10;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonFireNext
+            // 
+            this.buttonFireNext.Enabled = false;
+            this.buttonFireNext.Location = new System.Drawing.Point(120, 53);
+            this.buttonFireNext.Name = "buttonFireNext";
+            this.buttonFireNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonFireNext.TabIndex = 11;
+            this.buttonFireNext.Text = "Fire Next";
+            this.buttonFireNext.UseVisualStyleBackColor = true;
+            this.buttonFireNext.Click += new System.EventHandler(this.buttonFireNext_Click);
+            // 
+            // buttonFireTimerWithID
+            // 
+            this.buttonFireTimerWithID.Enabled = false;
+            this.buttonFireTimerWithID.Location = new System.Drawing.Point(201, 53);
+            this.buttonFireTimerWithID.Name = "buttonFireTimerWithID";
+            this.buttonFireTimerWithID.Size = new System.Drawing.Size(115, 23);
+            this.buttonFireTimerWithID.TabIndex = 12;
+            this.buttonFireTimerWithID.Text = "Fire Timer With ID 1";
+            this.buttonFireTimerWithID.UseVisualStyleBackColor = true;
+            this.buttonFireTimerWithID.Click += new System.EventHandler(this.buttonFireTimerWithID_Click);
+            // 
+            // labelExampleCommands
+            // 
+            this.labelExampleCommands.AutoSize = true;
+            this.labelExampleCommands.Location = new System.Drawing.Point(12, 58);
+            this.labelExampleCommands.Name = "labelExampleCommands";
+            this.labelExampleCommands.Size = new System.Drawing.Size(102, 13);
+            this.labelExampleCommands.TabIndex = 13;
+            this.labelExampleCommands.Text = "Example Commands";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 362);
+            this.Controls.Add(this.labelExampleCommands);
+            this.Controls.Add(this.buttonFireTimerWithID);
+            this.Controls.Add(this.buttonFireNext);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.labelIP);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.labelFeedBack);
+            this.Controls.Add(this.labelCommand);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.textBoxFeedback);
+            this.Controls.Add(this.textBoxCommand);
+            this.Controls.Add(this.textBoxPort);
+            this.Name = "Form1";
+            this.Text = "API Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.TextBox textBoxFeedback;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Label labelCommand;
+        private System.Windows.Forms.Label labelFeedBack;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonFireNext;
+        private System.Windows.Forms.Button buttonFireTimerWithID;
+        private System.Windows.Forms.Label labelExampleCommands;
+    }
+}
+
---
+++ CueTimerAPIClient/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CueTimerAPIClient
+{
+    public partial class Form1 : Form
+    {
+        NetworkStream nwStream;
+        bool listening;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient(textBoxIP.Text, int.Parse(textBoxPort.Text));
+            nwStream = client.GetStream();
+            listening = true;
+            Task.Run(() =>
+            {
+                while (listening)
+                {
+                    byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                    int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                    Invoke((MethodInvoker)delegate () { 
+                        textBoxFeedback.Text = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead); 
+                    });
+                }
+                client.Close();
+            });
+            buttonDisconnect.Enabled = true;
+            buttonFireNext.Enabled = true;
+            buttonFireTimerWithID.Enabled = true;
+            buttonSend.Enabled = true;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            SendCommand(textBoxCommand.Text);
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            listening = false;
+        }
+
+        private void buttonFireNext_Click(object sender, EventArgs e)
+        {
+            SendCommand("FireNext");
+        }
+
+        private void buttonFireTimerWithID_Click(object sender, EventArgs e)
+        {
+            // This will send 1 as a parameter
+            // A timer with this id should be exist on the list before sending the command
+            SendCommand("FireTimerWithID#1");
+        }
+
+        private void SendCommand(string command)
+        {
+            Console.WriteLine("Sending : " + command);
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(string.Format("{0}$",command));
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+        
+    }
+}
